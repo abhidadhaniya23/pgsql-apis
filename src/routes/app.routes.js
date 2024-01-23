e639a3249e7090f0c82f90f65661a5f80f9e271a
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2024-01-23 16:09:19 +0530

    removed unwanted routes

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,12 +1,8 @@
 import express from 'express'
 import userRouter from './user.routes.js'
-import productRouter from './product.routes.js'
-import orderRouter from './order.routes.js'
 
 const appRouter = express.Router()
 
 appRouter.use('/user', userRouter)
-appRouter.use('/product', productRouter)
-appRouter.use('/order', orderRouter)
 
 export default appRouter
--- src/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const productRouter = express.Router()
-
-productRouter.get('/', (req, res) => {
-  res.send('Product route')
-})
-
-export default productRouter
